3c2ac1ea9a5d60b4e0574a7cbc528798dcc6c420
TheEgghead27 <45245358+TheEgghead27@users.noreply.github.com>
2021-03-17 15:30:39 -0400

    Initial commit

---
+++ not_as_simple_squares.c
@@ -0,0 +1,12 @@
+/*something about calculating and printing square numbers for chapter 1 first c program*/
+/* Program prog1a.c: print the numbers from 4 to 9 and their squares */
+
+#include <stdio.h>
+main() 
+{
+	int number, sqnumber;
+	for (number = 4; number <= 9; number = number + 1) {
+		sqnumber =  number * number;
+		printf("%d %d\n", number, sqnumber);
+	};
+};
---
+++ simple_squares.c
@@ -0,0 +1,26 @@
+/*something about calculating and printing square numbers for chapter 1 first c program*/
+/* Program prog1a.c: print the numbers from 4 to 9 and their squares */
+
+#include <stdio.h>
+main() 
+{
+	int number, sqnumber;
+	number =  4;
+	sqnumber =  number * number;
+	printf("%d %d\n", number, sqnumber);
+	number =  5;
+        sqnumber =  number * number;
+        printf("%d %d\n", number, sqnumber);
+	number =  6;
+        sqnumber =  number * number;
+        printf("%d %d\n", number, sqnumber);
+	number =  7;
+        sqnumber =  number * number;
+        printf("%d %d\n", number, sqnumber);
+	number =  8;
+        sqnumber =  number * number;
+        printf("%d %d\n", number, sqnumber);
+	number =  9;
+        sqnumber =  number * number;
+        printf("%d %d\n", number, sqnumber);
+};
