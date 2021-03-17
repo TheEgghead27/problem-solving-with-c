67b9572d5c475379e522cad0238f69b83cb7fec5
TheEgghead27 <45245358+TheEgghead27@users.noreply.github.com>
2021-03-17 19:12:06 -0400

    when the exercises are lengthy!

--- bug_hunting.c
+++ bug_hunting.c
@@ -1,10 +1,10 @@
-/* comant is ok {;
-#includ <stdio>;
-main
-{
-	ints x,y;
+/* comment is ok */
+#include <stdio.h>
+int main(void) {
+	int x, y;
 
-		x + 1 = 4;
-		y = y + 1
-		x = 3y + 5x
-		printf(x,y)
+	x = 1 + 4;
+	y = x + 1;
+	x = (3 * y) + (5 * x);
+	printf("%d, %d", x, y);
+}
---
+++ nass_mod.c
@@ -0,0 +1,59 @@
+/*something about calculating and printing square numbers for chapter 1 first c program*/
+/* Program prog1a.c: print the numbers from 4 to 9 and their squares */
+
+#include <stdio.h>
+int main(void)
+{
+	int number, sqnumber;
+	// exercise 14
+	for (number = 4; number <= 9; number = number++) {
+		printf("%d %d\n", number, sqnumber); // huh nice it grabs garbage data for the uninitialized sqnumber
+		sqnumber = number * number;
+	};
+	// exercise 15
+	printf("******\n");
+	for (number = 4; number <= 9; number = number++) {
+		sqnumber = number * number;
+		printf("%d ", sqnumber);
+	};
+	// exercise 16
+	printf("******\n");
+	for (number = 4; number <= 9; number = number++) {
+		sqnumber = number * number;
+		printf("%d %d %d\n", number, sqnumber, (sqnumber * number));
+	};
+	// exercise 17
+	printf("******\n");
+	for (number = 4; number <= 9; number = number++) {
+		sqnumber = number * number;
+		printf("%d %d %d %d %d\n", number, sqnumber, (sqnumber * number), (number * 2), (number *3));
+	};
+	// exercise 18
+	printf("******\n");
+	for (number = 4; number <= 20; number += 2) {
+		sqnumber = number * number;
+		printf("%d %d\n", number, sqnumber);
+	};
+	// exercise 19
+	printf("******\n");
+	for (number = 4; number <= 20; number += 2) {
+		sqnumber = number * number;
+		printf("%d %d\n", number, sqnumber);
+	};
+	int cbnumber;
+	for (number = 5; number < 20; number += 2) {
+		cbnumber = number * number * number;
+		printf("%d %d\n", number, cbnumber);
+	};
+	// exercise 20
+	printf("******\n");
+	for (number = 4; number <= 9; number = number++) {
+		sqnumber = number * number;
+		if (number > 5) {
+			printf("%d %d\n", number, sqnumber);
+		} else
+		{
+			printf("%d\n", number);
+		};
+	};
+};
