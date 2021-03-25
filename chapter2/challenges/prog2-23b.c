7a97df56193b50a9fd36e9cdc056e93c7e4e3c37
TheEgghead27 <45245358+TheEgghead27@users.noreply.github.com>
2021-03-25 19:05:56 -0400

    challenged 20-23

--- chapter2/challenges/prog2-23b.c
+++ chapter2/challenges/prog2-23b.c
@@ -5,14 +5,18 @@
  */
 
 #include <stdio.h>
+#include <limits.h>
 
 int main(void) {
 	printf("Table of Function Values\n\n");
 	printf("Grade Point Average\tRange Covered\tValue of Formula\tStatus\n\n");
-	double gpa, result;
+	double gpa, result, worstGPA, worstResult;
+	worstGPA = INT_MAX;
 	for (gpa = 0.00; gpa <= 4.00; gpa += 0.50) {
 		result = (gpa * gpa * gpa + 7 * gpa - 1) / (gpa * gpa - (gpa + 5) / 3);
-		printf("\t%.2f\t\t%.2f - %.2f\t%f\t", gpa, gpa, (gpa + 0.24), result);	
+		if (result < worstGPA)
+			worstGPA = gpa, worstResult = result;
+		printf("\t%.2f\t\t%.2f - %.2f\t%f\t", gpa, gpa, (gpa + 0.24), result);
 		if (result > -5)
 			printf("\tAccepted");  // ok nvm c isnt indent based you dummy the scope is just up to the next semicolon
 		else if (result < -5)
@@ -21,5 +25,5 @@ int main(void) {
 			printf("\t See professor");
 		printf("\n");
 	};
-	printf("\nThe table is finished\n");
+	printf("\nThe table is finished, with the worst result being %.2f from a GPA of %.2f\n", worstResult, worstGPA);
 };
