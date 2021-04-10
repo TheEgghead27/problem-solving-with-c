cda3498f72c8513defe929c70ed9fc9be558a190
TheEgghead27 <45245358+TheEgghead27@users.noreply.github.com>
2021-04-10 14:57:51 -0400

    why have i been doing this for hours

--- chapter3/prog3.c
+++ chapter3/prog3.c
@@ -7,8 +7,12 @@
 
 #include <stdio.h>
 int main(void) {
-	int id, numemps=0;
+	// bloat bloat bloat
+	int id, numemps=0, unionBool;
 	double hours, rate, pay;
+	double totalHours=0.0, totalPay=0.0;  // mama im sorry they did not teach me arrays yet
+	int richest = -1111, bestPaid = -1111, hardestWorking = -1111;
+	double bestPay = 0.0, bestRate = 0.0, bestSlave = 0.0;
 	double tax, netpay;
 
 	printf("This program computes an employee's pay.\n");
@@ -20,18 +24,53 @@ int main(void) {
 
 		printf("Enter the hours worked> ");
 		scanf("%lf", &hours);
+		if (hours > bestSlave) {
+			bestSlave = hours;
+			hardestWorking = id;
+		};
 		printf("Enter the rate of pay> ");
 		scanf("%lf", &rate);
-		pay = hours * rate;
-		tax = (pay < 300) ? (0.15 * pay) : (0.28 * pay);
+		if (rate > bestRate) {
+			bestRate = rate;
+			bestPaid = id;
+		};
+		printf("Enter the employee's union status (1 for union, 0 for nonunion)> ");
+		scanf("%d", &unionBool);
+		if (hours < 40 || (unionBool != 1))
+			pay = hours * rate;
+		else
+			pay = (40 * rate) + ((hours - 40) * (rate * 1.5));
+		tax = pay * 0.09;
+		if (unionBool == 1)
+			tax *= 1.1;
+		if (pay >= 100) {
+			tax += 20;
+		}
+		else {  // fucking fuck fuck fuck its yandev time
+			if ((pay - tax) > 10)
+				tax = pay;  // xixixi government takes everything
+			else
+				tax += 10;
+		}
+
+		/* tax = (pay < 300) ? (0.15 * pay) : (0.28 * pay); */
 		netpay = pay - tax;
 
+		if (pay > bestPay) {
+			bestPay = pay;
+			richest = id;
+		};
+
 		printf("Employee %d worked %4.1f hours ", id, hours);
-		printf("at a rate of $%5.2f, earning $%7.2f\n", rate, pay);
-		printf("tax withheld was $%7.2f, leaving net pay of $%7.2f\n\n", tax, netpay);
-		numemps++;
+		printf("at a rate of $%-5.2f, earning $%-7.2f.\n", rate, pay);
+		if (unionBool == 1)
+			printf("$%.2f in union dues paid and ", rate, pay);	
+		printf("tax withheld was $%-7.2f, leaving net pay of $%-7.2f\n\n", tax, netpay);
+		numemps++; totalHours += hours; totalPay += pay;
 		printf("Enter the employee's ID number> ");
 		scanf("%d", &id);
 	};
-	printf("We have processed %d employees\n", numemps);
+	printf("We have processed %d employees, with a total number of hours worked being %.2f and %.2f of pay.\n", numemps, totalHours, totalHours);
+	printf("\t\t\t\tAmount\t\tEmployee\n");
+	printf("Highest Pay Rate\t\t$%.2f\t\t%d\nHighest Pay Amount\t\t$%.2f\t\t%d\nMost Hours Worked\t\t$%.2f\t\t%d", bestRate, bestPaid, bestPay, richest, bestSlave, hardestWorking);
 };
