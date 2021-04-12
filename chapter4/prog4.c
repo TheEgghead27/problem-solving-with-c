d65de9f79d8e559c3907d829f6e0f67fbae2b90d
TheEgghead27 <45245358+TheEgghead27@users.noreply.github.com>
2021-04-12 11:43:47 -0400

    prog4b

--- chapter4/prog4.c
+++ chapter4/prog4.c
@@ -1,13 +1,13 @@
 /* 
  * Program prog4.c
- * find the sum of the first 30 squares
+ * find the sum of the first 27 squares
  */
 #include <stdio.h>
 
 int main(void) {
 	int item, sum = 0;
 
-	for (item = 1; item <= 30; item++)
+	for (item = 1; item <= 27; item++)
 		sum += item * item;
-	printf("%d is the sum of the first 30 squares\n", sum);
+	printf("%d is the sum of the first 27 squares\n", sum);
 }
