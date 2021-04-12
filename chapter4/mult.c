f0b5a49b12f8e0d125fedd660b56d94abaeea091
TheEgghead27 <45245358+TheEgghead27@users.noreply.github.com>
2021-04-12 11:58:01 -0400

    mult.c progress

---
+++ chapter4/mult.c
@@ -0,0 +1,14 @@
+/*
+ * Program mult.c
+ * construct a multiplication table,
+ * showing the first 10 multiples of the numbers from 1 to 10
+ */
+#include <stdio.h>
+
+int main(void) {
+	int m1, m2;
+
+	printf("This is a Multiplication Table, from 1 to 10\n");
+	printf("\n")
+	printf("  X")
+}
