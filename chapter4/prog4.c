df158dc1e49613e5e2bd9dfb3068b13713ebbbbc
TheEgghead27 <45245358+TheEgghead27@users.noreply.github.com>
2021-04-12 11:53:08 -0400

    prog4d

--- chapter4/prog4.c
+++ chapter4/prog4.c
@@ -4,12 +4,16 @@
  * with NUMBERTOSUM defined as a constant
  */
 #include <stdio.h>
-#define NUMBERTOSUM 27  // haha the book says the int limit is 32767
+// #define NUMBERTOSUM 27  // haha the book says the int limit is 32767
 
 int main(void) {
 	int item, sum = 0;
+	int numbertosum;
 
-	for (item = 1; item <= NUMBERTOSUM; item++)
+	printf("type in the number of squares to be summed> ");
+	scanf("%d", &numbertosum);
+
+	for (item = 1; item <= numbertosum; item++)
 		sum += item * item;
-	printf("%d is the sum of the first %d squares\n", sum, NUMBERTOSUM);
+	printf("%d is the sum of the first %d squares\n", sum, numbertosum);
 }
