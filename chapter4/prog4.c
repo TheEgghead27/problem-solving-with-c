1e10e9321354fca781b9025ad99e79b561153b1d
TheEgghead27 <45245358+TheEgghead27@users.noreply.github.com>
2021-04-12 11:50:29 -0400

    prog4c

--- chapter4/prog4.c
+++ chapter4/prog4.c
@@ -1,13 +1,15 @@
 /* 
  * Program prog4.c
- * find the sum of the first 27 squares
+ * find the sum of the first NUMBERTOSUM squares
+ * with NUMBERTOSUM defined as a constant
  */
 #include <stdio.h>
+#define NUMBERTOSUM 27  // haha the book says the int limit is 32767
 
 int main(void) {
 	int item, sum = 0;
 
-	for (item = 1; item <= 27; item++)
+	for (item = 1; item <= NUMBERTOSUM; item++)
 		sum += item * item;
-	printf("%d is the sum of the first 27 squares\n", sum);
+	printf("%d is the sum of the first %d squares\n", sum, NUMBERTOSUM);
 }
