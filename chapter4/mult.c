23cab43afcc762db03a5b2a64f87aa7f7a8c3f01
TheEgghead27 <45245358+TheEgghead27@users.noreply.github.com>
2021-04-13 11:41:11 -0400

    mult.c finished

--- chapter4/mult.c
+++ chapter4/mult.c
@@ -9,6 +9,13 @@ int main(void) {
 	int m1, m2;
 
 	printf("This is a Multiplication Table, from 1 to 10\n");
-	printf("\n")
-	printf("  X")
+	printf("\n");
+	printf("  X    1    2    3    4    5    6    7    8    9    10\n");
+	for (m1 = 1; m1 <= 10; m1++) {
+		printf("\n");
+		printf("%3d", m1);
+		for (m2 = 1; m2 <= 10; m2++)
+			printf("%5d", m1 * m2);
+		printf("\n");
+	};
 }
