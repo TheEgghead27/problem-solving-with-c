63909c78a18cefc046b9081c136edb3191398404
TheEgghead27 <45245358+TheEgghead27@users.noreply.github.com>
2021-04-26 11:52:09 -0400

    32d.c with stolen randRange function

--- chapter5/32.c
+++ chapter5/32.c
@@ -4,6 +4,7 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+int randRange(int lower, int upper);
 
 
 int main(void) {
@@ -11,7 +12,12 @@ int main(void) {
 	
 	srand(69);
 	for (i = 1; i <= 10; i++) {
-		n = rand();
+		n = randRange(1, 6);
 		printf("%d\n", n);
 	}
+}
+
+// stolen from geeksforgeeks
+int randRange(int lower, int upper) {
+	return ((rand() % (upper - lower + 1)) + lower);
 }
