7d21fab234c11827d6d819187da70c95aa944724
TheEgghead27 <45245358+TheEgghead27@users.noreply.github.com>
2021-08-16 16:30:00 -0400

    Globals (chapter 12)

---
+++ chapter12/globals.c
@@ -0,0 +1,7 @@
+//
+// Around the world, around the world, around the world, around the world, around the world...
+//                                                                                      - Daft Punk
+//
+#include <stdio.h>
+int x = 1, y = 1, z = 1;
+
